ade252fca863e1d040b78130549d54544511604a
Cybertoge <trejo.allan@yahoo.com>
2021-02-03 22:32:10 -0800

    styledComponents

--- captureReact/capture/src/App.js
+++ captureReact/capture/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyle from "./components/GlobalStyled";
 import AboutUs from "./Pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle></GlobalStyle>
       <AboutUs></AboutUs>
     </div>
   );
--- captureReact/capture/src/components/AboutSection.js
+++ captureReact/capture/src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//Styles
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
+    <About>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -18,18 +20,27 @@ const AboutSection = () => {
             <h2>true</h2>
           </div>
         </div>
-
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills
         </p>
         <button>Contact Us</button>
-        <div className="image">
-          <img src={home1} alt="" />
-        </div>
       </div>
-    </div>
+      <div className="image">
+        <img src={home1} alt="" />
+      </div>
+    </About>
   );
 };
+
+//Styled Component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
 
 export default AboutSection;
