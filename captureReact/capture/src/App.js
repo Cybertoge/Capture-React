3bcc5a4854698395abbecdbc1b6fa8022d8a3e44
Cybertoge <trejo.allan@yahoo.com>
2021-02-16 21:19:38 -0800

    FramerMotion

--- captureReact/capture/src/App.js
+++ captureReact/capture/src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import GlobalStyle from "./components/GlobalStyled";
 import AboutUs from "./Pages/AboutUs";
+import Nav from "./components/Nav";
+import ContactUs from "./Pages/ContactUs";
+import OurWork from "./Pages/OurWork";
+import { Switch, Route } from "react-router-dom";
+import MovieDetail from "./Pages/MovieDetails";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <AboutUs></AboutUs>
+      <GlobalStyle />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
